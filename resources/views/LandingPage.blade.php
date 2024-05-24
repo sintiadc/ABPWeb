1da1ac90c70271b7a7de30832a1bf87e5479fec9
sintiadc <sintiadc@student.telkomuniversity.ac.id>
2024-05-25 01:58:07 +0700

    Commit43

--- resources/views/LandingPage.blade.php
+++ resources/views/LandingPage.blade.php
@@ -8,15 +8,18 @@
         <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/LandingPage.css">
+    <link rel="stylesheet" href="/css/Home.css">
+
 
 
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light px-5">
             <img class="navbar-brand" src="/image/brand.png" alt="Brand Image">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
@@ -61,6 +64,7 @@
     </div>
 
 
+    
     <script>
         $(document).ready(function(){
             $('.navbar-toggler').click(function(){
@@ -68,5 +72,9 @@
             });
         });
     </script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
